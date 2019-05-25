2c45c7bd3e43ba32537a4827b8b5f144f7f29b16
Tien Nguyen <tien.ng1020@gmail.com>
2019-05-25 00:19:04 -0500

    updated

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -28,3 +28,59 @@ var questionAndAnswers = [{
 ]
 
 setTimeout(thirtyseconds,1000*30);
+
+//  Interval Demonstration
+    //  Set our number counter to 100.
+    var number = 100;
+
+    //  Variable that will hold our interval ID when we execute
+    //  the "run" function
+    var intervalId;
+
+    //  When the stop button gets clicked, run the stop function.
+    $("#stop").on("click", stop);
+
+    //  When the resume button gets clicked, execute the run function.
+    $("#resume").on("click", run);
+
+    //  The run function sets an interval
+    //  that runs the decrement function once a second.
+    //  *****BUG FIX******** 
+    //  Clearing the intervalId prior to setting our new intervalId will not allow multiple instances.
+    function run() {
+      clearInterval(intervalId);
+      intervalId = setInterval(decrement, 1000);
+    }
+
+    //  The decrement function.
+    function decrement() {
+
+      //  Decrease number by one.
+      number--;
+
+      //  Show the number in the #show-number tag.
+      $("#show-number").html("<h2>" + number + "</h2>");
+
+
+      //  Once number hits zero...
+      if (number === 0) {
+
+        //  ...run the stop function.
+        stop();
+
+        //  Alert the user that time is up.
+        alert("Time Up!");
+      }
+    }
+
+    //  The stop function
+    function stop() {
+
+      //  Clears our intervalId
+      //  We just pass the name of the interval
+      //  to the clearInterval function.
+      clearInterval(intervalId);
+    }
+
+    //  Execute the run function.
+    run();
