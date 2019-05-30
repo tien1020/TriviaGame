7f41ee839cdea79bde4d721f1132211a9e839d64
Tien Nguyen <tien.ng1020@gmail.com>
2019-05-29 22:52:38 -0500

    updated

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,22 +15,22 @@ var userPick;
 var questionAnswers = [{
   question: "What is the seventh planet from the sun?",
   answers: ["1. Neptune", "2. Mercury", "3. Earth", "4. Uranus"],
-  rightAnswer: answers[3],
+  rightAnswer: 3,
 
 }, {
   question: "What is the world's longest river?",
   answers: ["1. Amazon", "2. Nile", "3. Mekong", "4. Missouri"],
-  rightAnswer: answers[0],
+  rightAnswer: 0,
   animateImage: "https://media3.giphy.com/media/pYvP6Bf0Uhtm0/200w.webp?cid=790b76115ce7298e785a396d7364905b&rid=200w.webp",
 }, {
   question: "What is the diameter of Earth?",
   answers: ["1. 12.000 miles ", "2. 10.000 miles", "3. 8.000 miles", "4. 15.000 miles"],
-  rightAnswer: answers[2],
+  rightAnswer: 2,
   animateImage: "https://media3.giphy.com/media/pYvP6Bf0Uhtm0/200w.webp?cid=790b76115ce7298e785a396d7364905b&rid=200w.webp",
 }, {
   question: "May Queen, Wisley Crab, Foxwhelps and Lane's Prince Albert are all species of what?",
   answers: ["1. Oranges", "2. Apples", "3. Pinnapples", "4. Strawberries"],
-  rightAnswer: answers[1],
+  rightAnswer: 1,
 }];
 
 
@@ -40,145 +40,167 @@ var questionAnswers = [{
 
 function timer() {
   clearInterval(intervalId);
-  intervalId = setInterval(decrement, 1000)}
+  intervalId = setInterval(decrement, 1000)
+}
 
-  function decrement() {
-    numberSeconds--;
-    $("#timer").html("<h2>" + numberSeconds + "seconds left</h2>");
-    if (numberSeconds === 0) {
-      $("#questions").hide();
-      $("#submit-button").hide();
-      stopTimer();
-      checkAnswer();
-      displayResults();
-    }
-    else if (numberSeconds == 0) {
-      stopTimer();
-      stopGame();
-      outOfTime(questionCount)
-      displayResults();
-      setTimeout(function () {
-        nextQuestion();
-        resetTimer();
-      }, 5000);
-      return;
-
-    }
-  }
-  
-
-  $("#start-button").on("click", startTimer);
-  function startTimer() {
-    
-
-    $("#questions").show();
-    $("#submit-button").show();
-    timeId = setInterval(intervalId, 1000);
-    timer();
-    showTrivia();
-
-  }
-
-
-
-  function stopTimer() {
-    clearInterval(intervalId);
+function decrement() {
+  numberSeconds--;
+  $("#timer").html("<h2>" + numberSeconds + "seconds left</h2>");
+  if (numberSeconds === 0) {
     $("#questions").hide();
     $("#submit-button").hide();
+    stopTimer();
+    checkAnswer();
+    displayResults();
+  }
+  else if (numberSeconds == 0) {
+    stopTimer();
+    stopGame();
+    outOfTime(questionCount)
+    displayResults();
+    setTimeout(function () {
+      nextQuestion();
+      resetTimer();
+    }, 5000);
+    return;
 
   }
+}
 
 
-  function resetTimer() {
-    numberSeconds = 30;
-    $("#timer").text(numberSeconds);
-    timer();
-  }
+$("#start-button").on("click", startTimer);
+function startTimer() {
 
 
+  $("#questions").show();
+  $("#submit-button").show();
+  timeId = setInterval(intervalId, 1000);
+  timer();
+  showTrivia();
+
+}
+
+
+
+function stopTimer() {
+  clearInterval(intervalId);
+  $("#questions").hide();
+
+}
+
+
+function resetTimer() {
+  numberSeconds = 30;
+  $("#timer").text(numberSeconds);
+  timer();
   
+}
 
 
-  function displayResults (){
-    $("#results").text("You answered correctly "+ correctAnswers + " questions out of 4");
-    $("#rightAnswer").text("The correct answer is: " + questionAnswers.rightAnswer);
 
-  }
 
-  function resetGame (){
+
+function displayResults() {
+  $("#results").text("You answered correctly " + correctAnswers + " questions out of 4");
+  $("#rightAnswer").text("The correct answer is: " + questionAnswers[index].rightAnswer);
+
+}
+
+function resetGame() {
   $("#questions").hide();
   $("#submit-button").hide();
   $("#results").hide();
-  // resetTimer();
-  // startTimer();
-  // showTrivia();
+  resetTimer();
+
+}
+
+
+
+// var indexQuestion = questionAnswer
+// console.log(indexQuestion);
+
+
+// $("#submit-button").on("click", submit);
+
+// function submit() {
+//   $("#question").hide();
+//   $("#submit-button").hide();
+//   displayResults();
+// }
+
+function showTrivia() {
+  $("#question").html(questionAnswers[index].question);
+  console.log(questionAnswers)
+  for (var i = 0; i < questionAnswers[index].answers.length; i++) {
+    var newDiv = $("<div>");
+    newDiv.html(questionAnswers[index].answers[i]);
+    newDiv.attr("data-index",i);
+    newDiv.attr("data-right-answer",questionAnswers[index].rightAnswer);
+    newDiv.addClass("option");
+    $("#answers").append(newDiv);
   }
+}
 
 
-  
-  // var indexQuestion = questionAnswer
-  // console.log(indexQuestion);
 
 
-  function showTrivia() {
-    $("#question").html(questionAnswers[index].question);
-    for(var i=0; i< questionAnswers[index].answers.length;i++){
-      $("#answers").append(questionAnswers[index].answers[i]+"<br><br>")
-    }
-    
-    
- 
-  //     var answerArr = questionAnswers.answers;
-  //     var buttonsArr = [];
 
-  // for (i = 0; i < questionAnswers.answers.length; i++) {
-  //   var button = $('<button>');
-  //   button.text(answerArr[i]);
-  //   button.attr('data-id', i);
-  //   $('#answers').append(button);
-  //  }
-   $('#answers').on('click', function {
-   
-    userPick = $(this).data("id");
-    questionAnswers.answers;
-    if(userPick != questionAnswers.rightAnswer) {
-   
+//     var answerArr = questionAnswers.answers;
+//     var buttonsArr = [];
+
+// for (i = 0; i < questionAnswers.answers.length; i++) {
+//   var button = $('<button>');
+//   button.text(answerArr[i]);
+//   button.attr('data-id', i);
+//   $('#answers').append(button);
+//  }
+
+
+$('#answers').on('click',".option", function () {
+console.log(this);
+  userPick = newDiv;
+  if (data-index != data-right-answer) {
+
     $('#answers').text("Wrong Answer!");
     incorrectAnswers++;
-   
-   }
-    else if (userPick === questionAnswers.rightAnswer) {
-   $('#answers').text("Correct!!!");
-   correctAnswers++;
-   
-   }
-   $("#question").hide();
-    $("#submit-button").hide();
-    
-    displayResults();
-    stopTimer();
-    return questionAnswers;
 
   }
-  
-);
+  else if (data-index === data-right-answer) {
+    $('#answers').text("Correct!!!");
+    correctAnswers++;
 
-  
+  }
+  $("#question").hide();
+  $("#submit-button").hide();
+
+  displayResults();
+  stopTimer();
+  return questionAnswers;
+
+})
+
+
+
+
+
+
+
+
 // $("#next-question").on("click", nextQuestion);
 // function nextQuestion() {
 //   currentQuestion (); 
 //     nextQuestion = setInterval(currentQuestion, 1000 * 30);
 
 // }
-    
- 
-  
+
+
+
   //   function currentQuestion(){
   //     randomQuestion++;
   //     // updateScore();
   //     showTrivia (); 
   // }
-  
+
 
 
   // function selectQuestion() {
@@ -192,10 +214,10 @@ function timer() {
 
     // questionImage.src = questionObject.image_src;
 
-   
+
     // resetTimer();
 
-  
+
 
 
 
