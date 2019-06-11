91a0ff75ac37252ff9681a922a125c358eb12c07
Tien Nguyen <tien.ng1020@gmail.com>
2019-06-10 21:18:59 -0500

    updated

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,7 @@
 var correctAnswers = 0;
 var incorrectAnswers = 0;
 var unAnswered = 0;
-var index = 0;
+// var index = 0;
 var questionCount = 0;
 var timeId;
 var numberSeconds = 30;
@@ -10,7 +10,7 @@ var intervalId;
 var playQuestions;
 var currentQuestion;
 var userPick;
-
+var gameId;
 
 var questionAnswers = [{
   question: "What is the seventh planet from the sun?",
@@ -49,33 +49,35 @@ function decrement() {
   $("#timer").html("<h2>" + numberSeconds + "seconds left</h2>");
   if (numberSeconds === 0) {
     $("#questions").hide();
-    $("#submit-button").hide();
     stopTimer();
-    displayResults();
-  }
-  else if (numberSeconds == 0) {
-    stopTimer();
-    stopGame();
-    outOfTime(questionCount)
+    // increment unanswered questions 
+    // wait
+    // get a new question
     displayResults();
     setTimeout(function () {
       nextQuestion();
       resetTimer();
     }, 5000);
-    return;
-
   }
+  // else if (numberSeconds == 0) {
+  //   stopTimer();
+    
+   
+    
+
+  // }
 }
 
 
-$("#start-button").on("click", startTimer);
-function startTimer() {
+$("#start-button").on("click", startGame);
 
-
-  // $("#questions").show();
-  timeId = setInterval(intervalId, 1000);
+function startGame() {
+  questionCount =0;
+  resetTimer();
+correctAnswers =0;
   timer();
   showTrivia();
+  // gameId = setInterval(nextQuestion, 40000);
 
 }
 
@@ -83,27 +85,26 @@ function startTimer() {
 
 function stopTimer() {
   clearInterval(intervalId);
-  resetTimer();
-  if(questionCount < questionAnswers.length - 1) {
-      setTimeout(startTimer, 2000);
-      setTimeout(showTrivia, 3000);
+  // resetTimer();
+  // if(questionCount < questionAnswers.length - 1) {
+      // setTimeout(startGame, 5000);
+      // setTimeout(showTrivia, 5000);
   }
-}
-resetTimer();
+
+// resetTimer();
 
 
 function resetTimer() {
   numberSeconds = 30;
-  $("#timer").text(numberSeconds);
   timer();
   
 }
 
-function startGame() {
-  gameId = setInterval(nextQuestion, 30000);
-  startTimer();
-  showTrivia(questionCount);
-}
+// function startGame() {
+//   gameId = setInterval(nextQuestion, 40000);
+//   startTimer();
+//   showTrivia();
+// }
 
 
 
@@ -112,58 +113,34 @@ function startGame() {
 function displayResults() {
 
   $("#results").text("You answered correctly " + correctAnswers + " questions out of 4");
-  $("#rightAnswer").text("The correct answer is: " + questionAnswers[index].rightAnswer);
+  $("#rightAnswer").text("The correct answer is: " + questionAnswers[questionCount].answers[questionAnswers[questionCount].rightAnswer]);
 console.log("this should be the answer:", rightAnswer);
 }
 
-// function resetGame() {
-//   $("#questions").empty();
-//   $("#results").empty();
-//   $("#rightAnswer").empty();
-//   stopTimer();
-//   resetTimer();
-
-// }
 
 
 
 
 
-//function answeredCorrectly() {
-//   correctAnswer++
-//   say "you got it right"
-
-//   if currentQUestion=== questionAnswers.length -1
-//   setTimeout()
-//   continue to next question
-// }
-//   else currentQUestion=== questionAnswers.length
-// display end results
 
 
-
-// function answeredIncorrectly(){
-  // incorrectAnswers++
-  // say "wrong"
-  // if currentQUestion=== questionAnswers.length -1
-  //   setTimeout()
-  //   continue to next question
-//   else currentQUestion=== questionAnswers.length
-// display end results
-
-// }
 
 function nextQuestion() {
-    
-  questionCount++;
+  
   if (questionCount < questionAnswers.length) {
-      showTrivia(questionCount);
+    $("#question").empty();
+    
+    $("#results").empty();
+    $("#rightAnswer").empty();
+
+
+      showTrivia(questionAnswers[questionCount]);
+      resetTimer();
   }
   else {
       questionCount = 0;
-      endOfGame();
+      // endOfGame();
       stopTimer();
-      stopGame();
       console.log("end of array");
   }
 
@@ -172,20 +149,16 @@ function nextQuestion() {
 
 
 function showTrivia() {
-  $("#question").html(questionAnswers[index].question);
+  $("#question").html(questionAnswers[questionCount].question);
   console.log(questionAnswers)
-  for (var i = 0; i < questionAnswers[index].answers.length; i++) {
+  $("#answers").empty();
+  for (var i = 0; i < questionAnswers[questionCount].answers.length; i++) {
     var newDiv = $("<div>");
-    newDiv.html(questionAnswers[index].answers[i]);
+    newDiv.html(questionAnswers[questionCount].answers[i]);
     newDiv.attr("dataIndex",i);
-    newDiv.attr("dataRightAnswer",questionAnswers[index].rightAnswer);
+    newDiv.attr("dataRightAnswer",questionAnswers[questionCount].rightAnswer);
     newDiv.addClass("option");
     $("#answers").append(newDiv);
-    // timer();
-    // stopTimer();
-    // startGame();
-    // createResult(questionCount, newDiv);
-
   }
 }
 
@@ -199,45 +172,27 @@ $("#answers").on("click",".option", function (e) {
   if ($(this).attr('dataindex') === $(this).attr('datarightanswer')) {
     console.log('index',$(this).attr('dataindex'));
         $("#answers").text("Correct!!!");
+        correctAnswers++;
         displayResults();
         stopTimer();
-    correctAnswers++;
+    
     questionCount++;
   }
   else {
     $("#answers").text("Wrong Answer!");
+    incorrectAnswers++;
     displayResults();
     stopTimer();
-    incorrectAnswers++;
+    
     questionCount++;
 
   }
-//   setTimeout(function () {
-//     nextQuestion();
-//     timer.resetTimer();
-// }, 5000)
- 
-checkGameEnd();
+
+ setTimeout(nextQuestion, 5000);
+// checkGameEnd();
 
 });
 
-function checkGameEnd() {
-  if(questionCount === questionAnswers.length) {
-      $("timer").hide();
-      displayResults();
-      questionCount = 0;
-
-          resetResults();
-          startGame();
-      }
-  }
-
-
-function resetResults() {
-  correctAnswers = 0;
-  incorrectAnswers = 0;
-  unanswered = 0;
-}
 
 
 
